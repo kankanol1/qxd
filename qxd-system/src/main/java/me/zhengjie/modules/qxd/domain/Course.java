01cd08f04028d7f97628d129d4df5badf403afc0
lidianzhong <lidianzhong@gl-data.com>
2020-12-11 20:03:49 +0800

    Initial commit

---
+++ qxd-system/src/main/java/me/zhengjie/modules/qxd/domain/Course.java
@@ -0,0 +1,10 @@
+package me.zhengjie.modules.qxd.domain;
+
+/**
+ * @author kankan
+ * @version 1.0
+ * @created 2020-12-11 19:13 by kankan
+ * @updated 2020-12-11 19:13 by kankan
+ */
+public class Course {
+}
